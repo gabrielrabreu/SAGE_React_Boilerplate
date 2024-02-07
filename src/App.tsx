65d98bef04401658b9e92f74683bc4aa9a84688f
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-02-06 21:09:47 -0300

    Just testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Grid, GridItem, Card } from "./components";
 
 const App: React.FC = () => {
-  return <div data-testid="title">Hello World!</div>;
+  return (
+    <div style={{ backgroundColor: "#f5f5f5" }}>
+      <h1>My App</h1>
+      <Grid
+        xs={{ col: 3, row: 2 }}
+        sm={{ col: 4, row: 2 }}
+        md={{ col: 6, row: 2 }}
+        lg={{ col: 3, row: 2 }}
+        xl={{ col: 3, row: 3 }}
+      >
+        <GridItem xs={{ col: 1, row: 1 }} md={{ col: 1, row: 2 }} />
+        <GridItem xs={{ col: 1, row: 1 }}>
+          <div
+            style={{
+              display: "flex",
+              gap: "0.5rem",
+              backgroundColor: "red",
+              width: "100%",
+              height: "100%",
+              alignItems: "center",
+            }}
+          >
+            <p>Texto dentro do GridItem</p>
+            <button>Botão dentro do GridItem</button>
+          </div>
+        </GridItem>
+        <GridItem xs={{ col: 1, row: 1 }} md={{ col: 1, row: 2 }}>
+          <div>
+            <p>Outro conteúdo dentro do GridItem</p>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+            </ul>
+          </div>
+        </GridItem>
+        <GridItem>
+          <Card>
+            <p>Texto dentro do GridItem</p>
+            <button>Botão dentro do GridItem</button>
+          </Card>
+        </GridItem>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,32 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          }
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
     ],
   },
   plugins: [
