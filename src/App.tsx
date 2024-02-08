30c475f3c67310150403544fd0db1b1e6e09bcf8
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-02-08 10:12:14 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,63 @@
 import React from "react";
-import { Grid, GridItem, Card } from "./components";
+import { BellIcon, MailIcon, MenuIcon, UserIcon } from "lucide-react";
+
+import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
-    <div style={{ backgroundColor: "#f5f5f5" }}>
-      <h1>My App</h1>
-      <Grid
-        xs={{ col: 3, row: 2 }}
-        sm={{ col: 4, row: 2 }}
-        md={{ col: 6, row: 2 }}
-        lg={{ col: 3, row: 2 }}
-        xl={{ col: 3, row: 3 }}
-      >
-        <GridItem xs={{ col: 1, row: 1 }} md={{ col: 1, row: 2 }} />
-        <GridItem xs={{ col: 1, row: 1 }}>
-          <div
-            style={{
-              display: "flex",
-              gap: "0.5rem",
-              backgroundColor: "red",
-              width: "100%",
-              height: "100%",
-              alignItems: "center",
-            }}
-          >
-            <p>Texto dentro do GridItem</p>
-            <button>Botão dentro do GridItem</button>
+    <div className={styles["app"]}>
+      <header className={styles["navbar"]}>
+        <button className={styles["button-icon"]}>
+          <MenuIcon className={styles["icon"]} />
+        </button>
+        <button className={styles["brand"]}>
+          <p>Sage</p>
+        </button>
+        <ul className={styles["navbar-nav"]}>
+          <li>
+            <button className={styles["button-icon"]}>
+              <BellIcon className={styles["icon"]} />
+            </button>
+          </li>
+          <li>
+            <button className={styles["button-icon"]}>
+              <MailIcon className={styles["icon"]} />
+            </button>
+          </li>
+          <li>
+            <button className={styles["button-icon"]}>
+              <UserIcon className={styles["icon"]} />
+            </button>
+          </li>
+        </ul>
+      </header>
+      <main className={styles["main"]}>
+        <div className={styles["page-title"]}>
+          <h1>Form Elements</h1>
+          <nav>
+            <ol className={styles["breadcrumb"]}>
+              <li className={styles["breadcrumb-item"]}>
+                <a>Home</a>
+              </li>
+              <li className={styles["breadcrumb-item"]}>Forms</li>
+              <li className={styles["breadcrumb-item"]}>Elements</li>
+            </ol>
+          </nav>
+        </div>
+        <section className={styles["section"]}>
+          <div className={styles["row"]}>
+            <div className={styles["col-lg-6"]}>
+              <div className={styles["card"]}>
+                <div className={styles["card-body"]}>
+                  <h5 className={styles["card-title"]}>
+                    General Form Elements
+                  </h5>
+                </div>
+              </div>
+            </div>
           </div>
-        </GridItem>
-        <GridItem xs={{ col: 1, row: 1 }} md={{ col: 1, row: 2 }}>
-          <div>
-            <p>Outro conteúdo dentro do GridItem</p>
-            <ul>
-              <li>Item 1</li>
-              <li>Item 2</li>
-            </ul>
-          </div>
-        </GridItem>
-        <GridItem>
-          <Card>
-            <p>Texto dentro do GridItem</p>
-            <button>Botão dentro do GridItem</button>
-          </Card>
-        </GridItem>
-      </Grid>
+        </section>
+      </main>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+import "./index.scss";
+
 const rootElement = document.getElementById("root");
 if (rootElement !== null) {
   const root = ReactDOM.createRoot(rootElement);
